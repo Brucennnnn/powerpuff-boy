9aba41348608e1c6643b19c6a57e06fb457bb233
Brucennnnn <bruceoio@hotmail.com>
2025-03-28 17:36:56 +0700

    feat: add jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,26 @@
 import { Elysia } from "elysia";
 import { plugin } from "@backend/module";
 import cors from "@elysiajs/cors";
+import jwt from "@elysiajs/jwt";
 
 const app = new Elysia()
   .use(cors({ origin: "*" }))
+  .use(
+    jwt({
+      name: "jwt",
+      secret: "Fischl von Luftschloss Narfidort",
+    }),
+  )
+  .get("/sign/:name", ({ jwt, params: { name } }) => {
+    return jwt.sign({ name });
+  })
+  .get("/profile", async ({ jwt, error, headers: { authorization } }) => {
+    const profile = await jwt.verify(authorization);
+
+    if (!profile) return error(401, "Unauthorized");
+
+    return `Hello ${profile.name}`;
+  })
   .get("/hi", async () => {
     return "Hello World";
   })
